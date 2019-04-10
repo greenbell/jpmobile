8adaa27cf26180ba17e667dca324f7939a2cd198
msuzuki <piyohiko1011@gmail.com>
2019-04-10 14:47:09 +0900

    actionpack-3.2.22.5ではresolverの引数が増えているので落ちる。マイナーアップ。

--- lib/jpmobile/resolver.rb
+++ lib/jpmobile/resolver.rb
@@ -32,33 +32,36 @@ module Jpmobile
       File.expand_path(query, @path)
     end
 
-    def query(path, details, formats)
+    def query(path, details, formats, outside_app_allowed)
       query = build_query(path, details)
 
-      # deals with case-insensitive file systems.
-      sanitizer = Hash.new { |h,dir| h[dir] = Dir["#{dir}/*"] }
+      template_paths = find_template_paths(query)
+      template_paths = reject_files_external_to_app(template_paths) unless outside_app_allowed
 
-      template_paths = Dir[query].reject { |filename|
-        File.directory?(filename) ||
-          !sanitizer[File.dirname(filename)].include?(filename)
-      }
+      template_paths.map do |template|
+        handler, format, variant = extract_handler_and_format_and_variant(template)
+        contents = File.binread(template)
 
-      template_paths.map { |template|
-        handler, format = extract_handler_and_format(template, formats)
-        contents = File.binread template
+        virtual_path = if format
+                         if template =~ /.+#{path}(.+)\.#{format.to_sym.to_s}.*$/
+                           path.to_str + Regexp.last_match(1)
+                         else
+                           path.virtual
+                         end
+                       else
+                         path.virtual
+                       end
 
-        if format
-          variant = template.match(/.+#{path}(.+)\.#{format.to_sym.to_s}.*$/) ? $1 : ''
-          virtual_path = variant.blank? ? nil : path + variant
-        else
-          virtual_path = path.virtual
-        end
-
-        ActionView::Template.new(contents, File.expand_path(template), handler,
-          :virtual_path => virtual_path,
-          :format       => format,
-          :updated_at   => mtime(template))
-      }
+        ::ActionView::Template.new(
+          contents,
+          File.expand_path(template),
+          handler,
+          virtual_path: virtual_path,
+          format: format,
+          variant: variant,
+          updated_at: mtime(template),
+        )
+      end
     end
   end
 end
